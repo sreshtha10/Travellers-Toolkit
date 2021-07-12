faba83051f96cc9f646a06045b9b40b1c6811f4c
Sreshtha <sreshtha.mehrotra@gmail.com>
2021-07-12 22:15:15 +0530

    created UI for Language Fragment 1.1

--- app/src/main/java/com/sreshtha/conversionbuddy/ui/fragments/LanguageFragment.kt
+++ app/src/main/java/com/sreshtha/conversionbuddy/ui/fragments/LanguageFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.mlkit.nl.translate.TranslateLanguage
-import com.google.mlkit.nl.translate.TranslatorOptions
 import com.sreshtha.conversionbuddy.databinding.FragmentLanguageBinding
 
 class LanguageFragment : Fragment() {
